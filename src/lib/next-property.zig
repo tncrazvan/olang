21e38c33de33e2a8f92cf3803d7d0c02f4813f85
tncrazvan <tangent.jotey@gmail.com>
2023-06-25 19:24:03 +0200

    fixing and adding tests for next property

--- src/lib/next-property.zig
+++ src/lib/next-property.zig
@@ -6,12 +6,30 @@ const nextName = @import("./next-name.zig").nextName;
 
 const Property = struct { value: []const u8, next: []const u8 };
 
-pub fn nextProperty(payload: []const u8) []const u8 {
+pub fn nextProperty(payload: []const u8) !Property {
     const next = try nextPropertySeparator(payload);
     const name = try nextName(next);
 
     return Property{
         .value = name.value,
-        .next = name.value,
+        .next = name.next,
     };
+}
+
+test "property (1)" {
+    const property = try nextProperty(".myprop");
+    try expect(equals(u8, property.value, "myprop"));
+    try expect(equals(u8, property.next, ""));
+}
+
+test "property (2)" {
+    const property = try nextProperty(".myprop =");
+    try expect(equals(u8, property.value, "myprop"));
+    try expect(equals(u8, property.next, " ="));
+}
+
+test "property (3)" {
+    const property = nextProperty(" .myprop") catch Property{ .value = "", .next = "" };
+    try expect(equals(u8, property.value, ""));
+    try expect(equals(u8, property.next, ""));
 }
